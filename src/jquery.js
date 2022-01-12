6d31e98c7d21340de7026bd8e19e88e47e4e003a
weiranyi <weiranyi@vip.qq.com>
2022-01-12 17:17:25 +0800

    代码精简 jQuery中本身就只需要返回api，就不需要const api声明后再return啦

--- src/jquery.js
+++ src/jquery.js
@@ -1,15 +1,13 @@
 window.jQuery = function (selector) {
     const elements = document.querySelectorAll(selector);
-    // api可以操作elements,但不是返回的elements
-    const api ={
+    // 返回一个可以操作elements的api
+    return {
         // 闭包：函数访问外部变量
-      addClass(className){
-          for (let i = 0; i < elements.length; i++) {
-              elements[i].classList.add(className)
-          }
-          return api;// 能够进行链式操作关键是返回了同样的api对象
-      }
+        addClass(className) {
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].classList.add(className)
+            }
+            return api;// 能够进行链式操作关键是返回了同样的api对象，this指的就是api
+        }
     }
-
-    return api
 }
